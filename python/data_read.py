340d43cdba3915f108aa968fdbcc79236339e8c8
wifilipiak <61492916+wifilipiak@users.noreply.github.com>
2021-01-11 14:13:18 +0100

    Update data_read.py

---
+++ python/data_read.py
@@ -0,0 +1,20 @@
+import serial
+import time
+
+ser = serial.Serial()
+ser.baudrate = 115200
+ser.port = 'COM1'
+ser.parity = serial.PARITY_NONE
+ser.stopbits = serial.STOPBITS_ONE
+ser.bytesize = serial.EIGHTBITS
+ser.timeout = 1
+
+if ser.isOpen():
+    ser.close()
+ser.open()
+while True:
+    line = ser.readline()
+    print(line)
+    time.sleep(0.05)
+    if line == b'':
+        break
