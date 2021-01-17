f4a5c98750fbb6c13f03fc1d09361d5efac621e3
Artur Bauer <bauerart1@gmail.com>
2021-01-17 12:29:39 +0100

    Add threads

---
+++ python/FFS.py
@@ -0,0 +1,82 @@
+# This is a multithread program for data acquisition, filtration and visualization
+import threading
+import _thread
+import logging
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import time
+
+# For fake data generation
+import random
+
+import data_read
+
+
+class FFS:
+    def __init__(self, sleep=0.05):
+        self.lock = threading.Lock()
+        self.data = {}
+        self.gyroscope = [0, 0, 0]
+        self.sleep = sleep
+
+        self.start_acquisition()
+        # self.start_filtration()
+        self.start_visualization()
+
+    def start_acquisition(self):
+        # self.data_read = data_read.DataRead()
+        self.acquisitionThread = threading.Thread(target=self.acquisition)
+        self.acquisitionThread.start()
+        logging.info('Acquisition started')
+
+    def acquisition(self):
+        while(True):
+            self.lock.acquire()
+            # self.data = self.data_read.get_data()
+            self.gyroscope[0] += random.randint(-10, 10)
+            self.gyroscope[1] += random.randint(-10, 10)
+            self.gyroscope[2] += random.randint(-10, 10)
+            self.lock.release()
+            logging.debug(self.gyroscope)
+            time.sleep(self.sleep)
+
+    def start_filtration(self):
+        self.filtrationThread = threading.Thread(target=self.filtration)
+        self.filtrationThread.start()
+        logging.info('Filtration started')
+
+
+    def filtration(self):
+        # TODO
+        pass
+
+    def start_visualization(self):
+        self.visualizationThread = threading.Thread(target=self.visualization)
+        self.visualizationThread.start()
+        logging.info('Visualizations started')
+
+    def visualization(self):
+        fig = plt.figure()
+        ax = fig.add_subplot(111, projection='3d')
+        self.plotThread = _thread.start_new_thread(self.plotting, (fig, ax))
+        plt.show()
+
+    def plotting(self, fig, ax):
+        logging.info('Plotting started')
+        while True:
+            self.lock.acquire()
+            gyroscope = self.gyroscope
+            self.lock.release()
+            logging.debug(gyroscope)
+            ax.clear()
+            ax.quiver(0, 0, 0, gyroscope[0], gyroscope[1], gyroscope[2])
+            ax.set_xlim([-1, 1])
+            ax.set_ylim([-1, 1])
+            ax.set_zlim([-1, 1])
+            fig.canvas.draw_idle()  # use draw_idle instead of draw
+            time.sleep(self.sleep)
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    ffs = FFS(sleep=0.5)
--- python/data_read.py
+++ python/data_read.py
@@ -1,45 +1,107 @@
 import serial
 import time
+import threading
 
-ser = serial.Serial()
-ser.baudrate = 115200
-ser.port = 'COM1'
-ser.parity = serial.PARITY_NONE
-ser.stopbits = serial.STOPBITS_ONE
-ser.bytesize = serial.EIGHTBITS
-ser.timeout = 1
 
-ACC0_X = []
-ACC0_Y = []
-ACC0_Z = []
-GYRO_X = []
-GYRO_Y = []
-GYRO_Z = []
-ACC1_X = []
-ACC1_Y = []
-ACC1_Z = []
-MAG_X = []
-MAG_Y = []
-MAG_Z = []
+class DataRead:
+    def __init__(self):
+        self.ser = serial
+        self.ACC0_X = []
+        self.ACC0_Y = []
+        self.ACC0_Z = []
+        self.GYRO_X = []
+        self.GYRO_Y = []
+        self.GYRO_Z = []
+        self.ACC1_X = []
+        self.ACC1_Y = []
+        self.ACC1_Z = []
+        self.MAG_X = []
+        self.MAG_Y = []
+        self.MAG_Z = []
+        self.lock = threading.Lock()
 
-if ser.isOpen():
-    ser.close()
-ser.open()
-while ser.inWaiting() > 0:
-    line = ser.readline()
-    print(line)
-    sensors = line.split(b';')
-    ACC0_X.append(int(sensors[0].split(b',')[0].strip(b' ')))
-    ACC0_Y.append(int(sensors[0].split(b',')[1].strip(b' ')))
-    ACC0_Z.append(int(sensors[0].split(b',')[2].strip(b' ')))
-    GYRO_X.append(int(sensors[1].split(b',')[0].strip(b' ')))
-    GYRO_Y.append(int(sensors[1].split(b',')[1].strip(b' ')))
-    GYRO_Z.append(int(sensors[1].split(b',')[2].strip(b' ')))
-    ACC1_X.append(int(sensors[2].split(b',')[0].strip(b' ')))
-    ACC1_Y.append(int(sensors[2].split(b',')[1].strip(b' ')))
-    ACC1_Z.append(int(sensors[2].split(b',')[2].strip(b' ')))
-    MAG_X.append(int(sensors[3].split(b',')[0].strip(b' ')))
-    MAG_Y.append(int(sensors[3].split(b',')[1].strip(b' ')))
-    MAG_Z.append(int(sensors[3].split(b',')[2].strip(b' ')))
-    time.sleep(0.05)   
-ser.close()
+    def get_data(self):
+        self.lock.acquire()
+        response = {'ACC0': [self.ACC0_X, self.ACC0_Y, self.ACC0_Z]}
+        self.lock.release()
+        return response
+
+    def get_serial(self):
+        serial = serial.Serial()
+        serial.baudrate = 115200
+        serial.port = 'COM1'
+        serial.parity = serial.PARITY_NONE
+        serial.stopbits = serial.STOPBITS_ONE
+        serial.bytesize = serial.EIGHTBITS
+        serial.timeout = 1
+        return serial
+
+    def read_data(self):
+        ser.open()
+        while ser.inWaiting() > 0:
+            line = ser.readline()
+            print(line)
+            sensors = line.split(b';')
+            self.lock.acquire()
+            self.ACC0_X.append(int(sensors[0].split(b',')[0].strip(b' ')))
+            self.ACC0_Y.append(int(sensors[0].split(b',')[1].strip(b' ')))
+            self.ACC0_Z.append(int(sensors[0].split(b',')[2].strip(b' ')))
+            self.GYRO_X.append(int(sensors[1].split(b',')[0].strip(b' ')))
+            self.GYRO_Y.append(int(sensors[1].split(b',')[1].strip(b' ')))
+            self.GYRO_Z.append(int(sensors[1].split(b',')[2].strip(b' ')))
+            self.ACC1_X.append(int(sensors[2].split(b',')[0].strip(b' ')))
+            self.ACC1_Y.append(int(sensors[2].split(b',')[1].strip(b' ')))
+            self.ACC1_Z.append(int(sensors[2].split(b',')[2].strip(b' ')))
+            self.MAG_X.append(int(sensors[3].split(b',')[0].strip(b' ')))
+            self.MAG_Y.append(int(sensors[3].split(b',')[1].strip(b' ')))
+            self.MAG_Z.append(int(sensors[3].split(b',')[2].strip(b' ')))
+            self.lock.release()
+            time.sleep(0.05)   
+        ser.close()
+
+    def run(self):
+        if ser.isOpen():
+            ser.close()
+
+# ser = serial.Serial()
+# ser.baudrate = 115200
+# ser.port = 'COM1'
+# ser.parity = serial.PARITY_NONE
+# ser.stopbits = serial.STOPBITS_ONE
+# ser.bytesize = serial.EIGHTBITS
+# ser.timeout = 1
+
+# ACC0_X = []
+# ACC0_Y = []
+# ACC0_Z = []
+# GYRO_X = []
+# GYRO_Y = []
+# GYRO_Z = []
+# ACC1_X = []
+# ACC1_Y = []
+# ACC1_Z = []
+# MAG_X = []
+# MAG_Y = []
+# MAG_Z = []
+
+# if ser.isOpen():
+#     ser.close()
+# ser.open()
+# while ser.inWaiting() > 0:
+#     line = ser.readline()
+#     print(line)
+#     sensors = line.split(b';')
+#     ACC0_X.append(int(sensors[0].split(b',')[0].strip(b' ')))
+#     ACC0_Y.append(int(sensors[0].split(b',')[1].strip(b' ')))
+#     ACC0_Z.append(int(sensors[0].split(b',')[2].strip(b' ')))
+#     GYRO_X.append(int(sensors[1].split(b',')[0].strip(b' ')))
+#     GYRO_Y.append(int(sensors[1].split(b',')[1].strip(b' ')))
+#     GYRO_Z.append(int(sensors[1].split(b',')[2].strip(b' ')))
+#     ACC1_X.append(int(sensors[2].split(b',')[0].strip(b' ')))
+#     ACC1_Y.append(int(sensors[2].split(b',')[1].strip(b' ')))
+#     ACC1_Z.append(int(sensors[2].split(b',')[2].strip(b' ')))
+#     MAG_X.append(int(sensors[3].split(b',')[0].strip(b' ')))
+#     MAG_Y.append(int(sensors[3].split(b',')[1].strip(b' ')))
+#     MAG_Z.append(int(sensors[3].split(b',')[2].strip(b' ')))
+#     time.sleep(0.05)   
+# ser.close()
